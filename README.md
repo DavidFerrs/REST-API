46b40708bdf6425aac80f466087a815fa3ab2d12
David Silva <fernandes.-.david@hotmail.com>
2022-10-26 19:41:46 -0300

    working API

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # REST-API-
-Creating a REST API with GO
+Creating a REST API with Node
+
+## How to run
+
+- To run the aplication, you need to set up your database(Postgresql) and run on port 5432, i`ve use docker container to run a image of Postgresql
+- After set up the database you can type on terminal "yarn dev" or "npm dev" the server will be running on port 4003 in localhost
+- Now you can make requests, i recomend insomnia ou postman to make your requests and see working
---
+++ src/routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { CreateCategoryController } from "./controllers/CreateCategoryController";
+import { CreateProductController } from "./controllers/CreateProductController";
+
+const router = Router();
+
+const createProduct = new CreateProductController();
+const createCategory = new CreateCategoryController();
+
+router.post("/product", createProduct.handle);
+router.post("/category", createCategory.handle);
+router.get("")
+
+
+export {router}
